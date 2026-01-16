cb679c7e71d47317375be583bff8f6a343158f72
ganesh singh <ganeshsingh71680@gmail.com>
2026-01-16 23:16:00 +0530

    sadly not able to do anything for github

---
+++ Arrays/medium/Longest_Consecutive_Sequence.java
@@ -0,0 +1,12 @@
+package Arrays.medium;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class Longest_Consecutive_Sequence {
+    
+    public static void main(String[] args) {
+        int [] arr={0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+        solution(arr);
+    }
+}
